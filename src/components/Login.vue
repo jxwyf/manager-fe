73338aad2db22b7707100ca8c22bd9b9c4a48f91
lwq <2578831517@qq.com>
2021-07-12 23:03:30 +0800

    路由封装 熟悉vue3写法

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>欢迎来到登录页面</h1>
+    <el-button>回首页</el-button>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'welcome'
+  }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter,createWebHashHistory } from "vue-router";
+import Home from './../components/Home.vue'
+import Welcome from './../components/Welcome.vue'
+import Login from './../components/Login.vue'
+
+
+const routes = [
+    {
+        name:'home',
+        path:'/',
+        meta:{
+            title:'首页'
+        },
+        component:Home,
+        redirect:'./',  //重定向
+        children:[
+            {
+                name:'welcome',
+                path:'/welcome',
+                meta:{
+                    title:'欢迎页'
+                },
+                component:Welcome,
+            }, {
+                name:'login',
+                path:'/login',
+                meta:{
+                    title:'登录页'
+                },
+                component:Login,
+            }
+            
+
+        ]
+    }
+]
+
+
+const router = createRouter({
+    history: createWebHashHistory(), //哈希路由
+    routes
+})
+
+export default router;
