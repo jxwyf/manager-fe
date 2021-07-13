33be3e1390762a2b5613f722d363a6f92eaa5df4
lwq <2578831517@qq.com>
2021-07-13 23:09:32 +0800

    把element-plus配置到main.js里

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 
 const app = createApp(App);
 //加载router对象
-app.use(router).mount('#app')
+app.use(router).use(ElementPlus).mount('#app')
