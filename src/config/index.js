85d4778cb365f30ffd8f14629e58fb600aaf0e21
lwq <2578831517@qq.com>
2021-07-14 22:43:32 +0800

    环境配置封装

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.MODE || 'prod';
+const EnvConfig = {
+    dev:{
+        baseApi:'/',
+        mockApi:''
+    },
+    test:{
+        baseApi:'/',
+        mockApi:''
+    },
+    prod:{
+        baseApi:'/',
+        mockApi:''
+    },
+}
+export default {
+    env,
+    mock:true,
+    ...EnvConfig[env]
+
+}
